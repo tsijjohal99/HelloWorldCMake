d64b1aeca26c1e9c3ee05897e569229d4905ed3b
Jeevan Johal <j.johal@softwareinstitute.com>
2022-03-30 09:56:39 +0100

    initial commit

---
+++ build/CMakeFiles/say_hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/say_hello.dir/src/hello.cpp.obj"
+  "CMakeFiles/say_hello.dir/src/hello.cpp.obj.d"
+  "libsay_hello.a"
+  "libsay_hello.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/say_hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
